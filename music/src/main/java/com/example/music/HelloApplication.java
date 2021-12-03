8b3fcce5364896c6fe93543c37de9d7661b28fed
Likla Sinha <89774560+mjlmb-z@users.noreply.github.com>
2021-12-03 13:04:16 +0530

    Add files via upload

---
+++ music/src/main/java/com/example/music/HelloApplication.java
@@ -0,0 +1,28 @@
+package com.example.music;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.util.Objects;
+
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        // primaryStage.setTitle("Hello World")
+        primaryStage.setScene(new Scene(root,654,165));
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
